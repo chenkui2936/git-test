340c3aef38ff3d980c5f598931ba5be9a176973d
KK <1813916568@qq.com>
2018-11-30 00:31:50 +0800

    把本地项目添加到git

---
+++ src/helloword.java
@@ -0,0 +1,8 @@
+public class helloword {
+    public void say(){
+        System.out.println("aaaaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccccccccc");
+        System.out.println("dddddddddddddddddddd");
+    }
+}
